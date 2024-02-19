2b0725b67b7ed9046c43775bb0f0733d3c2443e4
João Assunção <joaoassuncao27@gmail.com>
2024-02-19 13:47:57 +0000

    Added the base Spring project that the API is built on

---
+++ src/main/java/com/github/ipmaweatherapi/IpmaWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.ipmaweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpmaWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpmaWeatherApiApplication.class, args);
+	}
+
+}
